18407a551ee3a786ab7ba2d860080e55dee7ea5d
tomas juarez <mokajua@gmail.com>
2022-08-10 14:44:05 -0300

    responsive js

--- index.js
+++ index.js
@@ -16,19 +16,31 @@ function mostrarMensaje (){
         
 }
 
-function ocultarMensaje(){
-    mensaje.style.display = 'none'
+
+
+const toggler = document.getElementById('button')
+
+const showMessage = ()=>{
+
+    if( mostrar===true ){
+        document.getElementById('logo1').style.display='none'
+        document.getElementById('imagenResponsive').style.display='block'
+        document.getElementById('textoResponsive').style.display='block'
+        console.log('el boton funciona')
+        mostrar=false
+    }else{
+        console.log('entra en el else')
+        document.getElementById('logo1').style.display='block'
+        document.getElementById('imagenResponsive').style.display='none'
+        document.getElementById('textoResponsive').style.display='none'
+        mostrar=true
+    }
     
+
 }
 
 
 
-btn.addEventListener("click", function(event){
-    if(event.target !=true){
-       document.getElementById('desplegar_menu').style.visibility="hidden"
-       mostrar = true;
-     }
- },true)
 
 
  
